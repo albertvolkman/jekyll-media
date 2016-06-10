27237d2d32c76dec56cf98c6ec25e00524cce39c
Albert Volkman <albert.volkman@gmail.com>
2016-06-10 16:38:00 -0400

    Add filename to MD5 to capture filename changes

--- media_file.rb
+++ media_file.rb
@@ -7,9 +7,10 @@ class MediaFile
   def initialize(f, site)
     file = File.stat(f)
     type = FileMagic.new.file(f).split(',').first
-    f.slice! site.dest
 
-    @id = Digest::MD5.hexdigest(f)
+    # Calculate MD5, prepend filename to catch file name changes.
+    @id = Digest::MD5.hexdigest(f + File.read(f))
+    f.slice! site.dest
     @name = f.split('/').last
     @path = f
     @size = Filesize.from("#{file.size} B").pretty
